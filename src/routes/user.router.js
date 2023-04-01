7863a357b3607e282fdf0efee7def6ff36e64262
Ednilson Fragoso <ednilson.fragoso@hotmail.com>
2023-03-31 23:59:10 -0300

    feat: add rota getById

--- src/routes/user.router.js
+++ src/routes/user.router.js
@@ -2,6 +2,7 @@ const route = require('express').Router();
 const users = require('../controller/user.controller')
 
 route.get('/', users.findAllUserController);
+route.get('/user/:id', users.findUserByIdController)
 
 
 module.exports = route;
