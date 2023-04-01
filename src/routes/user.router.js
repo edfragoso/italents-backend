aad8c9a0a0563e2ed87c7c2dcaa6082596eabaa1
Ednilson Fragoso <ednilson.fragoso@hotmail.com>
2023-04-01 13:57:56 -0300

    feat: add route create

--- src/routes/user.router.js
+++ src/routes/user.router.js
@@ -2,7 +2,8 @@ const route = require('express').Router();
 const users = require('../controller/user.controller')
 
 route.get('/', users.findAllUserController);
-route.get('/user/:id', users.findUserByIdController)
+route.get('/user/:id', users.findUserByIdController);
+route.post('/create', users.createdUserController)
 
 
 module.exports = route;
