a4ee94f4d0e162355a1f8101cdfe8e608a5db4a8
Ednilson Fragoso <ednilson.fragoso@hotmail.com>
2023-04-01 14:18:34 -0300

    refactor: add try/catch

--- src/controller/user.controller.js
+++ src/controller/user.controller.js
@@ -1,31 +1,44 @@
 const usersService = require('../service/user.service');
 const { body, validationResult } = require('express-validator');
 
-const findAllUserController = (req,res) => {
-    const users = usersService.findAllUsersService();
-
-    res.send(users);
-};
-
-const findUserByIdController = (req,res) => {
-    const paramId = req.params.id;
-    const chooseUser = usersService.findByIdUserService(paramId);
-    if(chooseUser !== undefined){
-        res.status(200).send(chooseUser)
-    } else {
-        res.status(404).send({message: 'User not found'})
+const findAllUserController = async (req, res) => {
+    try {
+        const users = await usersService.findAllUsersService();
+        res.send(users);
+    } catch (error) {
+        next(error);
     }
 };
 
-const createdUserController = (req,res) => {
-    const user = req.body;
-    if(user.name === undefined || user.lastname === undefined || user.email === undefined){
-        res.status(400).send({ message: "Incomplete data, please fill in all fields to proceed"})
-    } else {
-        const newUser = usersService.createdUserService(user);
-        res.status(200).send({ message: "user added successfully", data: newUser})
-    };
-}
+const findUserByIdController = async (req, res, next) => {
+    try {
+        const paramId = req.params.id;
+        const chooseUser = await usersService.findByIdUserService(paramId);
+        if (chooseUser !== undefined) {
+            res.status(200).send(chooseUser)
+        } else {
+            res.status(404).send({ message: 'User not found' })
+        }
+    } catch (error) {
+        next(error);
+    }
+};
+
+const createdUserController = async (req, res) => {
+    try {
+      const errors = validationResult(req);
+      if(!errors.isEmpty()){
+          res.status(400).send({ message: "Invalid or incomplete data", errors: errors.array() })
+      } else {
+          const user = req.body;
+          const newUser = await usersService.createdUserService(user);
+          res.status(200).send({ message: "User created successfully", data: newUser})
+      };
+    } catch (error) {
+      res.status(500).send({ message: "An error occurred while creating the user", error: error.message })
+    }
+  };
+  
 
 
 module.exports = {
