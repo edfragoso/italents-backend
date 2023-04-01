f4081fc5a62de9b9cab7e708575e02f7f5f91ca4
Ednilson Fragoso <ednilson.fragoso@hotmail.com>
2023-04-01 00:00:03 -0300

    feat: created findUserByIdController

--- src/controller/user.controller.js
+++ src/controller/user.controller.js
@@ -4,10 +4,20 @@ const findAllUserController = (req,res) => {
     const users = usersService.findAllUsersService();
 
     res.send(users);
+};
+
+const findUserByIdController = (req,res) => {
+    const paramId = req.params.id;
+    const chooseUser = usersService.findByIdUserService(paramId);
+    if(chooseUser !== undefined){
+        res.status(200).send(chooseUser)
+    } else {
+        res.status(404).send({message: 'User not found'})
+    }
 }
 
 
 module.exports = {
     findAllUserController,
-    
+    findUserByIdController
 }
