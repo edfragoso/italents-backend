1c04d8e9c6297bbaa87fc57df836947b77662f68
Ednilson Fragoso <ednilson.fragoso@hotmail.com>
2023-04-01 14:19:11 -0300

    refactor:  add express-validator/check

--- src/routes/user.router.js
+++ src/routes/user.router.js
@@ -1,9 +1,15 @@
 const route = require('express').Router();
 const users = require('../controller/user.controller')
+const { body } = require('express-validator/check');
 
 route.get('/', users.findAllUserController);
 route.get('/user/:id', users.findUserByIdController);
-route.post('/create', users.createdUserController)
+route.post('/create',
+  body('name').notEmpty(),
+  body('lastname').notEmpty(),
+  body('email').isEmail(),
+  users.createdUserController
+);
 
 
 module.exports = route;
